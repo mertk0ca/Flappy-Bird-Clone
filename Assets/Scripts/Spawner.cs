0bb7ffcc826ea6f7948be7f920e4049ad2776fce
Mert <144450322+mertk0ca@users.noreply.github.com>
2024-05-26 16:23:52 +0300

    Initial commit

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Birdie BirdScript;
+
+    public GameObject Borular;
+
+    public float height;
+
+    public float time;
+
+
+    private void Start()
+    {
+        StartCoroutine(SpawnObject(time));
+    }
+
+
+    public IEnumerator SpawnObject(float time)
+    {
+        while(!BirdScript.isDead)
+        {
+            Instantiate(Borular, new Vector3(3, Random.Range(-height, height), 0), Quaternion.identity);
+
+            yield return new WaitForSeconds(time);
+        }
+        
+    }
+
+
+}
